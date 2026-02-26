1cee090b1bd7fa99ceab4fa4af44cfbbc34c3211
Mohammed Al-Fattahi <mohammedalfattahi87@.com>
2026-02-26 03:01:04 +0300

    use prod settings on render

--- ibb_guide/wsgi.py
+++ ibb_guide/wsgi.py
@@ -8,9 +8,8 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ibb_guide.settings.base')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ibb_guide.settings.prod')
 
 application = get_wsgi_application()
